f12e071c10a378269e1e48feeca27f40ba7a885f
kamal-sethi <kamalsethi018@gmail.com>
2025-02-15 14:44:20 +0530

    created login and signup routes

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,7 @@
+export const loginController=async(req,res)=>{
+
+}
+
+export const signupController=async(req,res)=>{
+    
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
-import  express from "express";
+import express from "express";
+import authRoutes from "../routes/auth.route.js";
 
 const app = express();
 
+app.use("/api/auth", authRoutes);
 const PORT = 5001;
 
 app.listen(PORT, () => {
